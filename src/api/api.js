4bcb083c369c3ed848ce5ba516929475fc9cce3b
chenli <1q2w3e4r>
2020-12-08 09:01:57 +0800

    12

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,14 @@
 import Axios from '@/utils/request'
+import { Form } from 'vant';
 
 // 初始化
 export function ping() {
   return Axios.post('/myCard');
+}
+
+// 通用上传接口
+export function common_upload(file) {
+  const form = new FormData();
+  form.append('file', file);
+  return Axios.post('/common/upload', form);
 }
